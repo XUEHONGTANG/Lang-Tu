3b3be07ff55166ff6fe73cc389502b5f2afff8f3
nirose22 <smile85264@gmail.com>
2022-09-05 09:35:26 +0800

    header修改

--- js/header.js
+++ js/header.js
@@ -1,14 +1,4 @@
 
-
-// menubtn = document.getElementsByClassName('.top_nav_icon_menu-button');
-// active = document.getElementsByClassName('.active');
-
-// menubtn.addEventListener('click',toggleClass)
-
-// function toggleClass (e){
-    
-// }
-
 $('.top_nav_icon_menu-button').click (function(){
     $(this).toggleClass('active');
     $('#menu').toggleClass('open');
