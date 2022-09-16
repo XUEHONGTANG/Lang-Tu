0b9f631a28cae601122b2d74f7bf700b25d54043
meteorgem <qseawd87512360@gmail.com>
2022-09-16 15:23:24 +0800

    9/16 RJ 小圖換大圖JS 寵物頁面注意事項RWD修改

---
+++ js/petpage.js
@@ -0,0 +1,14 @@
+new Vue({
+    el:'#petPageApp',
+    data: {
+        petimg: 0,
+        imgList:[
+            "./images/Meteor/dog4.png",
+            "./images/Meteor/dog5.png",
+            "./images/Meteor/dog6.png",
+            "./images/Meteor/dog7.png",
+            "./images/Meteor/dog8.png",
+        ]
+    }
+    
+})
