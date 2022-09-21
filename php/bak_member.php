73a0b87204b73bc2dda97931656549d58af86873
SANADAYUAN <liyuan04367@gmail.com>
2022-09-21 16:38:25 +0800

    0921 後台會員頁面渲染

--- php/bak_member.php
+++ php/bak_member.php
@@ -1,15 +1,24 @@
 <?php
-     //MySQL相關資訊
-   $db_host = "127.0.0.1";
-   $db_user = "root";
-   $db_pass = "liyuan0142";
-   $db_select = "LangTu.member";
 
-   //建立資料庫連線物件
-   $dsn = "mysql:host=".$db_host.";dbname=".$db_select.";charset=utf8";
+include("./DB.php");
 
-   //建立PDO物件，並放入指定的相關資料
-   $pdo = new PDO($dsn, $db_user, $db_pass);
 
    // $pdo->exec('');
+   $data = json_decode(file_get_contents("php://input"), true); //接收前端傳來的json格式
+
+   //建立SQL
+   $sql = " SELECT * from member ";
+
+   $statement = $pdo->prepare($sql);
+   $statement->execute();
+
+    //抓出全部且依照順序封裝成一個二維陣列
+    $data = $statement->fetchAll();
+    // if ($statement->rowCount() > 0){ //只要有撈到一筆資料 = 有撈到資料
+    //     $data["successful"] = true; //successful的屬性數值顯示 true        
+    // } else{   //如果沒有撈到資料...
+    //     $data["successful"] = false;
+    // }
+    
+    echo json_encode($data);
 ?>
