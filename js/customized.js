79ddd954e79f73ff2a156810048f9390b928ffd6
nirose22 <smile85264@gmail.com>
2022-09-26 14:09:56 +0800

    no message

--- js/customized.js
+++ js/customized.js
@@ -32,6 +32,7 @@ function update() {
       circle.classList.remove("active");
     }
   });
+
   const actives = document.querySelectorAll(".active");
   progress.style.width =
     ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
